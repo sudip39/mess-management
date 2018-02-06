35a002e9e68b9b5f79d75f16a9ff5fa414411ea1
sudip39 <sudipstudy39@gmail.com>
2018-02-07 05:20:47 +0530

    Monthly bill update problem has been solved

--- index.js
+++ index.js
@@ -191,6 +191,7 @@ app.post("/itemperday",isMessSake, function(req, res) {
                     break;
                 }
             }
+        }
     });
     timers.setTimeout(function(){
         for (let i = 0; i < Items.length; ++i) {
@@ -212,6 +213,46 @@ app.post("/itemperday",isMessSake, function(req, res) {
                 });
             }
         }
+        timers.setTimeout( function(){
+            let total=0;
+    let itemTotal=0;
+    ItemPerDay.findAll({
+        attributes: ["id", "qty"]
+    }).then(row => {
+        for(let i=0;i<row.length;i++)
+            {
+                itemTotal=0;
+                Item.findAll({
+                    attributes: ["name", "price"],
+                    where: {id: row[i].dataValues.id}
+                }).then(row1 => {
+                    itemTotal = (row1[0].dataValues.price*row[i].dataValues.qty);
+                    total += itemTotal;
+                });
+            }
+
+        timers.setTimeout(() => {
+            dailyBill.findAll({
+                where:{date : new Date().toDateString()}
+            }).then(row => {
+                if(row.length!=0) {
+                    dailyBill.update({totalBill: total}, {where : {
+                        date : row[0].dataValues.date
+                    }});
+                } else {
+
+                    dailyBill.create({
+                        date:new Date().toDateString(),
+                        month:
+                        new Date().toDateString().split(" ")[1],
+                        totalBill: total
+                    }).then(row => {
+
+                    });
+                }
+            });
+        }, 200);
+    });},200);
         res.redirect('/');
     },4000)
 });
